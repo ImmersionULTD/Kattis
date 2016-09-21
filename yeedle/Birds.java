8429c4a4bab57a82100dca4ff944ef9c3cbd8dfa
Abraham Neuwirth <Yeedle@users.noreply.github.com>
2016-09-20 21:09:47 -0400

    added conditions to prevent ArrayOutOfBounds

--- yeedle/Birds.java
+++ yeedle/Birds.java
@@ -6,23 +6,27 @@ public class Birds {
 
         int length = input.nextInt();
         int distance = input.nextInt();
+        int buffer = distance - 1;
         int n = input.nextInt();
 
         
         int[] wire = new int[length+1];
 
-        for (int i = 0; i< 6; i++) {
-            wire[i] = 1;
-            wire[length-i] = 1;
+        if (wire.length <= 12){
+        	for (int i :wire ) i = 1;
         }
-        
+        else{
+            for (int i = 0; i< 6 ; i++) {
+                wire[i] = 1;
+                wire[length-i] = 1;
+            }
+        }
 
         
         for (int i =0; i < n; i++)
         {
-            int buffer = distance - 1;
-            int position = input.nextInt();
             
+            int position = input.nextInt();     
             for(int j = position - buffer; j <= position + buffer; j++) {
                 wire[j] = 1;
             }
@@ -30,15 +34,16 @@ public class Birds {
         
     
         int count = 0;
-        for (int i =0; i < wire.length; i++)
+        for (int i =6; i < wire.length-6; i++)
         {
 
             if (wire[i] == 0) {
                 count++;
 
-                for (int j = i; j < i+distance; j++){
+                for (int j = i; j < i+distance && j < wire.length; j++){
 
                     wire[j] = 1;
+
                 }
             }
             
